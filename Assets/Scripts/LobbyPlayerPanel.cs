b529108b1fab469bcb3a3b8023eb6b7dfd2902c4
Colby Nolasco <nolasccr@mail.uc.edu>
2022-12-07 19:55:52 -0500

    Agh

--- Assets/Scripts/LobbyPlayerPanel.cs
+++ Assets/Scripts/LobbyPlayerPanel.cs
@@ -10,12 +10,21 @@ public class LobbyPlayerPanel : MonoBehaviour
     [SerializeField] protected TMPro.TMP_Text txtName;
     [SerializeField] protected TMPro.TMP_Text txtReady;
     [SerializeField] protected GameObject pnlColor;
+    [SerializeField] public Button btnKick;
     private bool isReady = false;
+
+    public event Action OnKickPlayer;
 
     public void Start() {
         SetReady(isReady);
+        btnKick.onClick.AddListener(OnKickPlayerClicked);
     }
 
+    private void OnKickPlayerClicked()
+    {
+        Debug.Log($"Panel for {txtName.text} kick button pressed");
+        OnKickPlayer.Invoke();
+    }
     public void SetName(string newName) {
         txtName.text = newName;
     }
